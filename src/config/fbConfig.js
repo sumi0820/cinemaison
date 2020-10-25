e00f4dbcfebfd77244ca3f1fa06acf9d0aaab545
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-10-25 15:27:09 +0100

    Update env file

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,24 +1,27 @@
-  import firebase from 'firebase/app'
-  import 'firebase/firestore'
-  import 'firebase/auth' 
-  
-  
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyABy_xVfM7ugq4-0f9Hc2mU5FGpbrTTbtM",
-    authDomain: "cinemaison-f1cd1.firebaseapp.com",
-    databaseURL: "https://cinemaison-f1cd1.firebaseio.com",
-    projectId: "cinemaison-f1cd1",
-    storageBucket: "cinemaison-f1cd1.appspot.com",
-    messagingSenderId: "123728569928",
-    appId: "1:123728569928:web:f3bb573c6b3d9d76845a7d",
-    measurementId: "G-XL5CBWVN55"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.firestore().settings({ timestampsInSnapshots: true });
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-  export default !firebase.apps.length 
+const API_KEY = process.env.API_KEY;
+const PROJECT_ID = process.env.PROJECT_ID;
+const APP_ID = process.env.APP_ID;
+const MEASUREMENT_ID = process.env.MEASUREMENT_ID;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "cinemaison-f1cd1.firebaseapp.com",
+  databaseURL: "https://cinemaison-f1cd1.firebaseio.com",
+  projectId: PROJECT_ID,
+  storageBucket: "cinemaison-f1cd1.appspot.com",
+  messagingSenderId: "123728569928",
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig).firestore()
   : firebase.app().firestore();
-
